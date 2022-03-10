de0f68e30f06a227094ecaa90c507d522fd0121c
camillapalombi <camilla.palombi14@gmail.com>
2022-03-10 16:48:52 +0100

    JS finito

--- js/script.js
+++ js/script.js
@@ -115,16 +115,44 @@ const arrIcons = [
 	}
 ];
 
-const iconsContainer = document.querySelector('.main-container');
-insertIcons(arrIcons);
-function insertIcons(arrIcons) {
-    arrIcons.forEach(element => {
-        const elementIcons = document.createElement('div');
-        elementIcons.classList.add('icon-box');
+//MILESTONE 1. e 2.
+const iconsContainer = document.querySelector('.main-container'); //container dove stamperò le icone
+
+function insertIcons(arrIcons, iconsContainer) {    //funzione stampa icone 
+	iconsContainer.innerHTML = '';    //azzeramento del container per evitare il raddoppiamento
+    arrIcons.forEach(element => {    //per ogni elemento di arrIcons fai questo:
+        const elementIcons = document.createElement('div');    //crea un elemento div
+        elementIcons.classList.add('icon-box');    //dagli classe icon-box
         elementIcons.innerHTML = `<i class="${element.family} ${element.prefix}${element.name}" style="color: ${element.color}"></i>
-        <div class="text-icon">${element.name}</div>`;
-        iconsContainer.append(elementIcons);
-    })
+        <div class="text-icon">${element.name}</div>`;    //inseriscigli questo codice dentro
+
+        iconsContainer.append(elementIcons);    //stampo icone nel DOM
+    });
 };
+
+insertIcons(arrIcons, iconsContainer);    //richiamo funzione stampa icone
+
+
+//MILESTONE 3.
+let selectIconsType = document.getElementById('selects-icons-type');   //select dell'html
+
+
+selectIconsType.addEventListener("change", function () {
+
+    let optionSelected = this.value;   //opzione selezionata
+
+	if (optionSelected != "all") {     //se l'option selezionata ha value diversa da "all" fai:
+		const filterIcons = arrIcons.filter((icon) => {     //aggiungi alla variabile filterIcons solo le icone di tipo uguale a quello selezionato
+		  if (icon.type == optionSelected) {
+			return true;
+		  }
+		});
+		insertIcons(filterIcons, iconsContainer); //mostra le icone filtrare per tipo
+	  } else {
+		insertIcons(arrIcons, iconsContainer); //mostra tutte le icone
+	  }
+	});
+
+
 
 
